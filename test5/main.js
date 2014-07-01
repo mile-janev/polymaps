2f46aa09d8f5b898b36c20ef8755045687cec255
Mile Janev <Mile Janev@Mj-laptop>
2014-07-01 10:17:49 +0200

    Using tsv files

--- test5/main.js
+++ test5/main.js
@@ -2,6 +2,8 @@ var po = org.polymaps;
 
 var map = po.map()
     .container(document.getElementById("map").appendChild(po.svg("svg")))
+    .center({lat: 41.918, lon: 22.120})
+    .zoom(8.50)
     .add(po.interact())
     .add(po.hash());
     
@@ -20,6 +22,8 @@ map.add(po.geoJson()
 map.add(po.compass()
     .pan("none"));
     
+    map.container().setAttribute("class", "YlOrRd");
+    
 /* Mile */
 var mile = tsv("json/mile.tsv")
     .key(function(l) { return l[1]; })
@@ -30,10 +34,18 @@ function load(e) {
   for (var i = 0; i < e.features.length; i++) {
     
     var feature = e.features[i],
-        region = feature.data.properties.title;
+        region = feature.data.properties.title
+        coeficient = mile[region]/mile['Vkupno'];
+        
+//        feature.element.setAttribute("class", "q" + ~~(coeficient*8) + "-" + 8);
+      
+    console.log(region);
 //    console.log(mile[region]);
-    console.log(mile['Vkupno']);
-    feature.element.setAttribute("id", "id"+i);
+    console.log(coeficient);
+    console.log(~~(coeficient*10));
+//    console.log(mile[region]);
+//    console.log(mile['Vkupno']);
+//    feature.element.setAttribute("id", "id"+i);
   }
 }
 
