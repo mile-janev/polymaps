96f4442179e418bad4f3c08b9c45051137b77838
Mile Janev <>
2014-07-01 16:15:53 +0200

    Version 1

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+/* Mile */
+var mile = tsv("json/mile.tsv")
+    .key(function(l) { return l[1]; })
+    .value(function(l) { return l[2].replace(/,/g, ""); })
+    .map();
+    
+/* Suzana */
+var suzana = tsv("json/suzana.tsv")
+    .key(function(l) { return l[1]; })
+    .value(function(l) { return l[2].replace(/,/g, ""); })
+    .map();
+
+var orderArray = new Array();
+
 var po = org.polymaps;
 
 var map = po.map()
@@ -24,42 +38,30 @@ map.add(po.compass()
     
     map.container().setAttribute("class", "YlOrRd");
     
-/* Mile */
-var mile = tsv("json/mile.tsv")
-    .key(function(l) { return l[1]; })
-    .value(function(l) { return l[2].replace(/,/g, ""); })
-    .map();
-    
-/* Suzana */
-var suzana = tsv("json/suzana.tsv")
-    .key(function(l) { return l[1]; })
-    .value(function(l) { return l[2].replace(/,/g, ""); })
-    .map();
-    
-var orderArray = new Array();
-
 function load(e) {
+
+    var orderArray = new Array();//Ponovo ja reinicijaliziram bidejki funkcijata se povikuva pri zoom
     
-  for (var i = 0; i < e.features.length; i++) {
-    var feature = e.features[i],
+    for (var i = 0; i < e.features.length; i++) {
+        var feature = e.features[i],
         region = feature.data.properties.title;
-        
+
         orderArray.push({name: region.trim(), val: mile[region].trim()});
-  }
-  
-  orderArray.sort(function(a,b) { return a.val - b.val; });
-  
-  for (var j=0; j<orderArray.length; j++){
-      
-      for (var i = 0; i < e.features.length; i++) {
+    }
+
+    orderArray.sort(function(a,b) { return a.val - b.val; });
+
+    for (var j=0; j<orderArray.length; j++){
+
+        for (var i = 0; i < e.features.length; i++) {
             var feature = e.features[i],
                 regionName = feature.data.properties.title;
-        
+
             if (regionName == orderArray[j].name){
                 feature.element.setAttribute("class", "q" + j + "-" + 8);
                 feature.element.setAttribute("title", regionName + ": " + orderArray[j].val);
             }
-      }
-  }
+        }
+    }
 }
 
